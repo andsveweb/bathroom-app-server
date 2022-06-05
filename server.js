7f567515e6dd5cf1b47ec992c36b025ecf0ede56
Andreas <andsveweb@gmail.com>
2022-06-05 08:28:05 +0200

    Readmefile

--- server.js
+++ server.js
@@ -6,19 +6,20 @@ const mongoose = require('mongoose');
 const config = require('./DB.js');
 const Route = require('./post.route');
 
+
+
 // Connect to Mongoose with error handling 
 mongoose.connect(config.DB, { useNewUrlParser: true }).then(
   () => { console.log('Database is connected') },
   err => { console.log('Can not connect to the database' + err) }
 );
-
 // Set the port  for the server 
 app.set('port', (process.env.PORT || 4000)); 
-// add cors to the application to allow calls from other domain
-app.use(cors()); 
 // support encoded bodies middleware for the application
 app.use(bodyParser.urlencoded({extended: true})); 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); 
+// add cors to the application to allow calls from other domain
+app.use(cors()); 
 // add the route to the application
 app.use('/posts', Route); 
 // listen to the port
